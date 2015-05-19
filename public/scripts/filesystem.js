0a01b6e71bbf43562373ede9fb70a11ce76c6c6b
Chris <chris@boddy.im>
2015-05-19 18:45:42 +0100

    Pretty prrint file  sizes.

--- public/scripts/filesystem.js
+++ public/scripts/filesystem.js
@@ -3,19 +3,34 @@ var File = React.createClass({
         render: function() {
                 var dateString =  new Date(this.props.time*1000).toGMTString()
         var glyphClass = "glyphicon "; 
-        glyphClass += this.props.isdir ? "glyphicon-folder-open" : "glyphicon-cloud-download";
+glyphClass += this.props.isdir ? "glyphicon-folder-open" : "glyphicon-cloud-download";
 
 return (<tr onClick={this.props.onClick} ref={this.props.path}>
         <td><span className={glyphClass}/>{"   "+this.props.path}</td>
-        <td>{this.props.size}</td>
+        <td>{File.sizeString(this.props.size)}</td>
         <td>{dateString}</td>
         </tr>);
         }
 });
 
 File.timeSort = function(left, right){return left.time - right.time;} 
+
 File.sizeSort = function(left, right){return left.size - right.size;} 
+
 File.pathSort = function(left, right){return left.path.localeCompare(right.path);} 
+
+File.sizes = [{count : 1, unit:"bytes"}, {count : 1024, unit: "kB"}, {count: 1048576 , unit : "MB"}, {count: 1073741824, unit:"GB" } ]
+
+File.sizeString =  function(sizeBytes){
+        var iUnit=0;
+        var count=0;
+        for (iUnit=0; iUnit < File.sizes.length;iUnit++) {
+                count = sizeBytes / File.sizes[iUnit].count;
+                if (count < 1024)
+                        break;
+        }
+        return "" + (count|0) +" "+ File.sizes[iUnit].unit;   
+}
 
 function buildGetChildrenUrl(path) {
         return  "children?path="+path;
@@ -32,7 +47,7 @@ function buildUploadUrl(path, name) {
 
 var FileList = React.createClass({
         getInitialState: function() {
-                return {paths : ["/"],
+                return {paths : ["/home/chris"],
                         files: [],
     sort: File.pathSort};
         },
@@ -76,29 +91,29 @@ var FileList = React.createClass({
     },
 
     uploadFile: function() {
-        return function (evt) {
-                var path = this.currentPath();
-                var readFile = evt.target.files[0];
-                var name = readFile.name;
-                console.log(readFile);
+            return function (evt) {
+                    var path = this.currentPath();
+                    var readFile = evt.target.files[0];
+                    var name = readFile.name;
+                    console.log(readFile);
 
-                var formData = new FormData();
-                formData.append("file", readFile, name);
+                    var formData = new FormData();
+                    formData.append("file", readFile, name);
 
-                var xhr = new XMLHttpRequest();
-                xhr.open('POST', buildUploadUrl(path, name) , true);
-                xhr.onreadystatechange=function()
-                {
-                        if (xhr.readyState != 4)
-                                return;
+                    var xhr = new XMLHttpRequest();
+                    xhr.open('POST', buildUploadUrl(path, name) , true);
+                    xhr.onreadystatechange=function()
+                    {
+                            if (xhr.readyState != 4)
+                                    return;
 
-                        if (xhr.status == 200) 
-                                alert("Successfully uploaded file "+ name +" to "+ path); 
-                        else
-                                console.log(request.status);
-                };
-                xhr.send(formData);
-        }.bind(this)
+                            if (xhr.status == 200) 
+                                    alert("Successfully uploaded file "+ name +" to "+ path); 
+                            else
+                                    console.log(request.status);
+                    };
+                    xhr.send(formData);
+            }.bind(this)
     },
 
     componentDidMount: function() {
@@ -109,7 +124,7 @@ var FileList = React.createClass({
             var uploadButton = document.getElementById("uploadButton")
                     uploadButton.onclick = this.onUpload;
             var uploadInput = document.getElementById("uploadInput")  
-            uploadInput.addEventListener("change", this.uploadFile(), false);
+                    uploadInput.addEventListener("change", this.uploadFile(), false);
     },
 
     updateSort: function(sort) {
@@ -149,11 +164,12 @@ var FileList = React.createClass({
                             return (<File onClick={onClick} path={f.path} isdir={f.isdir} size={f.size} time={f.time}/>)
             }.bind(this));
 
+            var sortGlyph = "glyphicon glyphicon-sort";
             return (<table className="table table-responsive table-striped table-hover">
                             <thead><tr>
-                            <th onClick={this.pathSort}>Path</th>
-                            <th onClick={this.sizeSort}>Size</th>
-                            <th onClick={this.timeSort}>Last modified time</th>
+                            <th onClick={this.pathSort}><button className="btn btn-default"><span className={sortGlyph}/>Path</button></th>
+                            <th onClick={this.sizeSort}><button className="btn btn-default"><span className={sortGlyph}/>Size</button></th>
+                            <th onClick={this.timeSort}><button className="btn btn-default"><span className={sortGlyph}/>Last modified time</button></th>
                             </tr></thead>
                             <tbody>
                             {files}
